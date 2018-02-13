f81eaa4d1f8ef631d2efddda1356faf8a97d1e51
whitefang57 <lemon_57@yahoo.com>
2018-02-12 16:21:01 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+// ==UserScript==
+// @name         Visible Textareas
+// @namespace    http://tampermonkey.net/
+// @version      0.2
+// @description  make textareas more visible
+// @downloadURL  https://raw.githubusercontent.com/whitefang57/pcMomUpdate/master/script.js
+// @author       You
+// @match        https://*.planningcenteronline.com/*
+// @grant        none
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js
+// ==/UserScript==
+
 (function() {
     'use strict';
 
